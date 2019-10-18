d83c5367d467fece088b7b1902bbf9c1fcec85b4
Pedro J. Martínez <pedrojmrt@gmail.com>
2019-10-18 21:31:04 +0200

    improving error handling

--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,8 @@
+// fixed url for whatsapp web
 export const WHATSAPP_WEB_URL = 'https://web.whatsapp.com';
+// log directory name
 export const LOG_DIR_NAME = 'logs';
+// user-agent string to use
 export const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36';
+// after error, timeout to gracefully close in miliseconds
+export const GRACEFULLY_CLOSE_TIMEOUT = 5000;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import puppeteer from 'puppeteer';
 
-import { USER_AGENT, WHATSAPP_WEB_URL } from './consts';
+import { USER_AGENT, WHATSAPP_WEB_URL, GRACEFULLY_CLOSE_TIMEOUT } from './consts';
 import logger  from './logger';
 import { isProd } from './utils';
 import { version } from '../package.json';
 
+let browser, page;
+
+// TODO - Keep trying until getting session code or until timeout
+//  - Observe and refresh session code when it changes
+//  - Exit application if no qrcode is scanned when whatsapp qr scanning timeouts
+// TODO - Store & retrieve pass sessions so it doesn't have to scan the qrcode again
+// TODO - Get all the incoming messages, download attachments if the message is an image/voice/video/document and store them into db.
+//  - Get each message
+//  - Check if it's raw text, if it's not download it and reference it in the db.
+// TODO - HTML Preview of the chats (v2)
+
 async function launch() {
-    const browser = await puppeteer.launch({
+    browser = await puppeteer.launch({
         headless: isProd,
     });
     logger.verbose("using %s", await browser.version());
-    const page = await browser.newPage();
+    page = await browser.newPage();
     logger.verbose("changing user-agent to '%s'", USER_AGENT);
 
     page.setUserAgent(USER_AGENT);
@@ -32,14 +43,17 @@ async function init() {
 
     let code;
     try {
-        const { page } = await launch();
+        await launch();
         code = await getSessionCode(page);
     } catch(e) {
         logger.error(e);
-        process.exit(1);
+        browser.close();
+        setTimeout(() => {
+            process.exit(1);
+        }, GRACEFULLY_CLOSE_TIMEOUT);
     }
 
-    logger.verbose("got session code '%s'", code)
+    logger.verbose("got session code '%s'", code);
 }
 
 init();
