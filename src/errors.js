a81a617e02504b9720f4b838db92dde4967b84da
Pedro J. Martínez <pedrojmrt@gmail.com>
2019-10-23 20:40:55 +0200

    cleaning index.js

---
+++ src/errors.js
@@ -0,0 +1,9 @@
+import logger from './logger';
+import { clearSession } from './utils';
+
+export function staleDataDetected() {
+    logger.error("Old session data detected. Your credentials may have expired or you have closed your session from your mobile phone. To recover from this state, the application will delete your session data file.");
+    logger.info("Exiting..");
+    clearSession();
+    exit(0);
+}
