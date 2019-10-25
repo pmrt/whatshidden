d8d040b05e8b35205ce0d992743b1bf28a7ac1d5
Pedro J. Martínez <pedrojmrt@gmail.com>
2019-10-25 20:33:46 +0200

    remove unneeded SCRIPTS_NEEDED_TO_REPORT_LOAD

--- src/consts.js
+++ src/consts.js
@@ -34,8 +34,6 @@ export const DATA_DIR = '.data';
 export const FULL_DATA_PATH = join(process.cwd(), DATA_DIR);
 // session file (with extension)
 export const SESSION_FILE = 'session.json';
-// number of scripts needed to report that whatsapp web has loaded
-export const SCRIPTS_NEEDED_TO_REPORT_LOAD = 10;
 // interval after which the app will check again the login status to prevent stale
 // session data. (milliseconds)
 export const LOGIN_CHECK_INTERVAL = 900e3;
