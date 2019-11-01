357f6d323cd298547dfe79118cb0f29ba61db159
Pedro J. Martínez <pedrojmrt@gmail.com>
2019-11-01 19:37:21 +0100

    unsafe_update now doesn't delete current chat dir

--- unsafe_update.sh
+++ unsafe_update.sh
@@ -1,8 +1,14 @@
 # UNSAFE UPDATE — THIS OVERWRITES THE CURRENT WORKING TREE
 
+# copy the chats
+cp -r chats ~/.whatshidden
 # fetch the last stable branch, but do not merge
 git fetch origin master
 # set working tree to the last commit
 git reset --hard origin/master
 # install dependencies again
 npm install
+# get the chats back
+cp -r ~/.whatshidden/chats .
+# remove the temporary directory
+rm -rf ~/.whatshidden
