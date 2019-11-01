576c758cdacd09b56a60e5ffd92d6f5e6b2b3124
Pedro J. Martínez <pedrojmrt@gmail.com>
2019-11-01 14:32:15 +0100

    updated error TOO_MANY_ATTEMTPS

--- src/errors.js
+++ src/errors.js
@@ -2,7 +2,7 @@ import logger from './logger.js';
 import { clearSession, exit } from './utils.js';
 
 export const PAGE_WONT_LOAD = "WhatsApp Web won't load. Your credentials may have expired or you have closed your session from your mobile phone. In an attempt to recover from this state, the application will delete your session data file"
-export const TOO_MANY_ATTEMPTS_TO_RECOVER_SESSION = "Too many attempts to recover session. You may be using WhatsApp Web on another device too much, WhatsApp Web only allows 1 session at a time";
+export const TOO_MANY_ATTEMPTS_TO_RECOVER_SESSION = "Too many attempts to recover session: session unrecoverable. You may be using WhatsApp Web on another device too much (WhatsApp Web only allows 1 session at a time) or an unrecoverable error has occurred";
 
 export function recover(msg) {
     logger.error(msg);
